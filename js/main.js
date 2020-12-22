28bd9c95f4cb3cbdc78ff45c0bcc5e0270675821
deinvis <davis-oliveira@hotmail.com.br>
2020-12-22 19:11:57 -0300

    arquivos testes

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var nome = "Davis";
+var anonasci = 1998;
+var anoatual = 2020;
+alert(nome + " tem " + (anoatual - anonasci) + " anos");
+var string = nome + " tem " + (anoatual - anonasci) + " anos";
+console.log(string.replace("anos", "anos de vida"));
+var lista = ["maça", "pêra", "laranja"];
+console.log(lista[1]);
