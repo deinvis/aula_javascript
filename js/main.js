0d0e220fd9f4b6824d91626b0ca9a60b79c07ad0
deinvis <davis-oliveira@hotmail.com.br>
2020-12-23 13:25:15 -0300

    funcoes e aplicacoes com HTML

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
+function subtracao (a, b){
+    return a - b;
+}
 
-var d = new Date();
-alert(d.getDate());
+function clicou (){
+    document.getElementById("agradecimento").innerText = "Obrigado por clicar";
+}
 
-var nome = "Davis";
-var anonasci = 1998;
-var anoatual = 2020;
-alert(nome + " tem " + (anoatual - anonasci) + " anos");
-var string = nome + " tem " + (anoatual - anonasci) + " anos";
-console.log(string.replace("anos", "anos de vida"));
-var lista = ["maça", "pêra", "laranja"];
-console.log(lista.toString());
-var pessoa = [{nomep : nome, idade : anoatual - anonasci, sexo : "M" }, {nomep: "Ereeny", idade: 21, sexo: "F"}];
-console.log(pessoa[1].nomep);
+function redirecionar (){
+    window.open("http://www.facebook.com");
+}
 
-for(var i = 0; i < pessoa.length; i++){
-    if(pessoa[i].nomep === "Ereeny"){
-        alert("gostosa");
-    }
-    else {
-        alert("feio");
+function trocar(elemento){
+    elemento.innerHTML = elemento.value.toString().toUpperCase();
+}
+
+function voltar(elemento){
+    elemento.innerHTML = elemento.value;
+}
+
+function funcaochange(elemento){
+    if (elemento.value === "sim") alert("tiamu");
+    else alert("nojenta");
+}
+
+function testes(){
+    var d = new Date();
+    alert(d.getDate());
+    var nome = "Davis";
+    var anonasci = 1998;
+    alert(nome + " tem " + subtracao(d.getFullYear(), anonasci) + " anos");
+    var string = nome + " tem " + subtracao(d.getFullYear(), anonasci) + " anos";
+    console.log(string.replace("anos", "anos de vida"));
+    var lista = ["maça", "pêra", "laranja"];
+    console.log(lista.toString());
+    var pessoa = [{nomep : nome, idade : d.getFullYear() - anonasci, sexo : "M" }, {nomep: "Ereeny", idade: 21, sexo: "F"}];
+    console.log(pessoa[1].nomep);
+    for(var i = 0; i < pessoa.length; i++){
+        if(pessoa[i].nomep === "Ereeny"){
+            alert("gostosa");
+        }
+        else {
+            alert("feio");
+        }
     }
 }
