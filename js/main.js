786c027d9e4a6155dd0916dcb6c8af8c8c15caf2
deinvis <davis-oliveira@hotmail.com.br>
2020-12-22 21:51:13 -0300

    testes com Date, dicionarios, loops e condicionais

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+
+var d = new Date();
+alert(d.getDate());
+
 var nome = "Davis";
 var anonasci = 1998;
 var anoatual = 2020;
@@ -5,4 +9,15 @@ alert(nome + " tem " + (anoatual - anonasci) + " anos");
 var string = nome + " tem " + (anoatual - anonasci) + " anos";
 console.log(string.replace("anos", "anos de vida"));
 var lista = ["maça", "pêra", "laranja"];
-console.log(lista[1]);
+console.log(lista.toString());
+var pessoa = [{nomep : nome, idade : anoatual - anonasci, sexo : "M" }, {nomep: "Ereeny", idade: 21, sexo: "F"}];
+console.log(pessoa[1].nomep);
+
+for(var i = 0; i < pessoa.length; i++){
+    if(pessoa[i].nomep === "Ereeny"){
+        alert("gostosa");
+    }
+    else {
+        alert("feio");
+    }
+}
